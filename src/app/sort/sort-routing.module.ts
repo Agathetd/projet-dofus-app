f0a2fea7c60411b7d5ea83323b8289d11b359e19
Agathetd <adrienduriez3@hotmail.fr>
2023-05-04 16:38:49 +0200

    update menu and not-found

--- src/app/sort/sort-routing.module.ts
+++ src/app/sort/sort-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SortComponent } from './sort.component';
+import { SortDetailsComponent } from './pages/sort-details/sort-details.component';
 
 
 const routes: Routes = [
   {
     path: '',
     component: SortComponent
+  },
+  {
+    path: ':id',
+    component: SortDetailsComponent
   }
  ];
 
