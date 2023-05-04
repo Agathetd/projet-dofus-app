4591dc8e3762723318092d1c77c76bf389d09d83
Agathetd <adrienduriez3@hotmail.fr>
2023-05-04 17:14:49 +0200

    firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
 
 @NgModule({
   declarations: [
@@ -15,6 +18,8 @@ import { CoreModule } from './core/core.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     CoreModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
   ],
 
   providers: [],
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'projet-dofus-ad',
+    appId: '1:1050702985841:web:74c1a9767bd549d7e239d0',
+    storageBucket: 'projet-dofus-ad.appspot.com',
+    apiKey: 'AIzaSyCrTXzgQW2VqoObeH3iR_cNCc_4YJ9tsdQ',
+    authDomain: 'projet-dofus-ad.firebaseapp.com',
+    messagingSenderId: '1050702985841',
+  },
   production: false,
    iutApiBaseUrl: 'https://projet-iut-api-mongodb.vercel.app'
 };
